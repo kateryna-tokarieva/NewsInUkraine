62e4f4cffa2e71d16c46f16e93c8bb2ec1a501da
kateryna-tokarieva <tokareva.kate@icloud.com>
2024-08-05 17:58:55 +0300

    Initial commit

--- NewsInUkraine/HomeView.swift
+++ NewsInUkraine/HomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
+struct HomeView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
@@ -82,5 +82,5 @@ private let itemFormatter: DateFormatter = {
 }()
 
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    HomeView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
 }
